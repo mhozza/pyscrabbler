996af318abe23914d5b6cd7ff4c56f618761129d
Michal Hozza <mhozza@gmail.com>
2020-09-12 21:23:52 +0100

    Add permutations lookup.

--- pyscrabbler/scrabbler.py
+++ pyscrabbler/scrabbler.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 from string_algorithms.trie import Trie
-from collections import deque
+from collections import deque, Counter
 
 
 def build_trie(words):
@@ -37,12 +37,27 @@ def print_subtree(word, trie, limit=None):
         for c, subnode in sorted(node.children.items()):
             if limit is not None and limit > 0:
                 if subnode.is_word:
+                    print(w)
                     limit -= 1
                 q.append((subnode, w + c))
 
 
-def find_permutations(letters, trie, limit=None):
-    pass
+def find_permutations(word, trie, use_all_letters=True, limit=None):
+    root = trie.root
+    letters = Counter(word)
+    q = deque([(root, "", letters)])
+
+    while len(q):
+        node, w, l = q.popleft()
+        for c, subnode in sorted(
+            filter(lambda item: l[item[0]] > 0, node.children.items())
+        ):
+            if limit is not None and limit > 0:
+                new_w = w + c
+                if (not use_all_letters or len(new_w) == len(word)) and subnode.is_word:
+                    print(new_w)
+                    limit -= 1
+                q.append((subnode, new_w, l - Counter(c)))
 
 
 def find_regex(regex, words, limit=None):
@@ -53,7 +68,11 @@ def main(args):
     trie, words = initialize_dictionary(args.dictionary)
     word = args.word
     if args.subtree:
-        print_subtree(word, trie, args.limit)
+        print_subtree(word, trie, limit=args.limit)
+    elif args.permutations:
+        find_permutations(
+            word, trie, use_all_letters=not args.allow_shorter, limit=args.limit
+        )
     else:
         print(find_word(word, trie))
 
@@ -68,4 +87,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "--subtree", action="store_true", help="Print all words starting by [word]"
     )
+    parser.add_argument(
+        "-p", "--permutations", action="store_true", help="Print all permutations"
+    )
+    parser.add_argument(
+        "--allow_shorter", action="store_true", help="Print all permutations"
+    )
+
     main(parser.parse_args())
